6582a19f44abfc566554adc7f12f7e8bd26d5413
Dinesh648 <87705747+Dinesh648@users.noreply.github.com>
2024-04-23 22:29:56 +0530

    Initial commit

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -1,8 +1,36 @@
 import logging
 import pandas as pd
 from zenml import step
-
-#to clean the data
+from src.data_cleaning import DataCleaning, DataDivideStrategy, DataPreprocessStrategy
+from typing_extensions import Annotated
+from typing import Tuple
+# to clean the data
 @step
-def clean_df(df: pd.DataFrame) -> None:
-    pass
+def clean_df(df: pd.DataFrame) -> Tuple[
+    Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.Series, "y_test"],
+]:
+    """
+    Args:
+        df: Raw data
+    Returns:
+        X_train: Training data
+        X_test: Testing data
+        y_train: Training labels
+        y_test: Testing labels
+    """
+    try:
+        process_strategy = DataPreprocessStrategy()
+        data_cleaning = DataCleaning(df, process_strategy)
+        processed_data = data_cleaning.handle_data()
+
+        divide_strategy = DataDivideStrategy()
+        data_cleaning = DataCleaning(processed_data, divide_strategy)
+        X_train, X_test, y_train, y_test = data_cleaning.handle_data()
+        logging.info("Data cleaning completed")
+        return X_train, X_test, y_train, y_test
+    except Exception as e:
+        logging.error(f"Error in data cleaning: {e}")
+        raise e
