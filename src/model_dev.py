2504527203785a8ad13ab9546ec61be0747fd380
Dinesh648 <87705747+Dinesh648@users.noreply.github.com>
2024-04-24 19:36:49 +0530

    Initial commit

---
+++ src/model_dev.py
@@ -0,0 +1,44 @@
+import logging
+from abc import ABC, abstractmethod
+from sklearn.linear_model import LinearRegression
+
+class Model(ABC):
+    """
+    Abstract class for all models.
+    """
+
+    @abstractmethod
+    def train(self, X_train, y_train):
+        """
+        Trains the model on the given data.
+        Args:
+            X_train: Training data.
+            y_train: Training labels.
+        Returns:
+            None
+        """
+        pass
+
+class LinearRegressionModel(Model):
+    """
+    Linear Regression Model
+    """
+    def train(self, X_train, y_train, **kwargs):
+        """
+        Trains the Linear Regression model on the given data.
+        Args:
+            X_train: Training data.
+            y_train: Training labels.
+            **kwargs: Additional keyword arguments.
+        Returns:
+            None
+        """
+        try:
+            reg = LinearRegression(**kwargs)
+            reg.fit(X_train, y_train)
+            logging.info("Linear Regression model trained.")
+            return reg
+        except Exception as e:
+            logging.error(f"Error training Linear Regression model: {e}")
+            raise e
+        
--- steps/model_train.py
+++ steps/model_train.py
@@ -1,17 +1,35 @@
 import logging
 import pandas as pd
 from zenml import step
+from src.model_dev import LinearRegressionModel
+from sklearn.base import RegressorMixin
+from .config import ModelNameConfig
 
 
 @step
-def train_model(df: pd.DataFrame) -> None:
+def train_model(
+    X_train: pd.DataFrame,
+    X_test: pd.DataFrame,
+    y_train: pd.DataFrame,
+    y_test: pd.DataFrame,
+    config: ModelNameConfig,
+) -> RegressorMixin:
     """
     Trains the model on the ingested data.
 
     Args:
-        df (pd.DataFrame): The ingested data.
+        X_train: pd.DataFrame,
+        X_test: pd.DataFrame,
+        y_train: pd.DataFrame,
+        y_test: pd.DataFrame,
 
     Returns:
-        None
+        RegressorMixin
     """
-    pass
+    model = None
+    if config.model_name == "LinearRegression":
+        model = LinearRegressionModel()
+        trained_model = model.train(X_train, y_train)
+        return trained_model
+    else:
+        raise ValueError(f"Model {config.model_name} not supported")
